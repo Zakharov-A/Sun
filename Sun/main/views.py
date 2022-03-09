356348c89042d3d275fc4cdf31324e0235efd8ef
Zakharov-A <redzed427@gmail.com>
2022-03-08 20:11:22 -0500

    Django_course_L_4

--- Sun/main/views.py
+++ Sun/main/views.py
@@ -1,10 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
-from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("<h4>Checking work</h4>")
+    return render(request, 'main/index.html')
+
 
 def about(request):
-    return HttpResponse("<h4>I'm very smart!</h4>")
+    return render(request, 'main/about.html')
