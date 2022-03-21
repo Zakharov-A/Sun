94370c8b0b1684e8e7a9a7ea22e493b7cea42493
Zakharov-A <redzed427@gmail.com>
2022-03-20 21:00:13 -0400

    Django_course_L_7_1_working_with_the_database_model_creation

---
+++ Sun/news/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+
+class Artiles(models.Model):
+    title = models.CharField('Название', max_length=50)
+    anons = models.CharField('Анонс', max_length=250)
+    full_tex = models.TextField('Статья')
+    date = models.DateTimeField('Дата публикации')
+
+    def __str__(self):
+        return self.title
