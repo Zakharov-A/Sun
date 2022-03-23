bd2824546cbf303824f2bdda7516cdf73b5e9c63
Zakharov-A <redzed427@gmail.com>
2022-03-23 08:17:53 -0400

    Django_course_L_7_2_finish

--- Sun/news/models.py
+++ Sun/news/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 
-class Artiles(models.Model):
+class Articles(models.Model):
     title = models.CharField('Название', max_length=50)
     anons = models.CharField('Анонс', max_length=250)
     full_tex = models.TextField('Статья')
@@ -9,3 +9,7 @@ class Artiles(models.Model):
 
     def __str__(self):
         return self.title
+
+    class Meta:
+        verbose_name = 'Новость'
+        verbose_name_plural = 'Новости'
