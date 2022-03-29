e27ebcdfa6371e54710c481b13269b50664f58c8
Zakharov-A <global user.email redzed427@gmail.com>
2022-03-29 23:47:33 +0300

    Py_Django_L_9_1

--- Sun/news/models.py
+++ Sun/news/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Articles(models.Model):
     title = models.CharField('Название', max_length=50)
     anons = models.CharField('Анонс', max_length=250)
-    full_tex = models.TextField('Статья')
+    full_text = models.TextField('Статья')
     date = models.DateTimeField('Дата публикации')
 
     def __str__(self):
--- Sun/news/views.py
+++ Sun/news/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
 from .models import Articles
+from .forms import ArticlesForm
 
 def news_home(request):
     news = Articles.objects.order_by('-date')
     return render(request, 'news/news_page.html', {'news': news})
+
+def create(request):
+    form = ArticlesForm()
+
+    data = {
+        'form': form
+    }
+
+    return render(request, 'news/create.html', data)
